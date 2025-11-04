a7cc59034f25599c21e4d0f3c7d0356bc3f41c67
Lars Plessing <56645452+larsplessing@users.noreply.github.com>
2025-11-04 09:39:43 +0100

    chore: gitHub action for hana hdi container (#25)
    * action and scripts
    
    * use generic values
    
    * review
    
    * Update .github/actions/hana-hdi-container/action.yml
    
    * move optionals to end of list
    
    ---------
    
    Co-authored-by: sjvans <30337871+sjvans@users.noreply.github.com>

---
+++ .github/scripts/hana-hdi-container/setup.js
@@ -0,0 +1,66 @@
+import { promisify } from 'node:util'
+const sleep = promisify(setTimeout)
+
+import { join as path_join } from 'node:path'
+import { writeFileSync } from 'node:fs'
+
+import { fetch_token } from './token.js'
+
+const token = await fetch_token()
+const headers = { authorization: `Bearer ${token}`, 'content-type': 'application/json' }
+
+const url = process.env.INPUT_SERVICE_MANAGER_URL
+
+const i_url = url + '/v1/service_instances'
+const instancePrefix = process.env.INPUT_INSTANCE_PREFIX
+const i_name = `${instancePrefix}_ci_${Math.random().toString(36).substring(2, 15)}`
+const servicePlanId = process.env.INPUT_SERVICE_PLAN_ID
+const i_options = {
+  method: 'POST',
+  headers,
+  body: JSON.stringify({
+    name: i_name,
+    service_plan_id: servicePlanId
+  })
+}
+const i_res = await fetch(i_url, i_options)
+const i_loc = i_res.headers.get('location')
+const service_instance_id = i_loc.split('/')[3]
+
+for (let i = 0; i < 60; i++) {
+  await sleep(1000)
+  const res = await fetch(url + i_loc, { method: 'GET', headers })
+  const { state } = await res.json()
+  if (state === 'succeeded') break
+}
+
+const b_url = url + '/v1/service_bindings'
+const b_name = i_name + '_binding'
+const b_options = {
+  method: 'POST',
+  headers,
+  body: JSON.stringify({
+    name: b_name,
+    service_instance_id
+  })
+}
+const b_res = await fetch(b_url, b_options)
+const b_loc = b_res.headers.get('location')
+const service_binding_id = b_loc.split('/')[3]
+
+for (let i = 0; i < 60; i++) {
+  await sleep(1000)
+  const res = await fetch(url + b_loc, { method: 'GET', headers })
+  const { state } = await res.json()
+  if (state === 'succeeded') break
+}
+
+const res = await fetch(b_url + '/' + service_binding_id, { method: 'GET', headers })
+const { credentials } = await res.json()
+
+const configPath = process.env.INPUT_CONFIG_PATH || ''
+const cdsrc = path_join(process.cwd(), configPath, '.cdsrc.json')
+writeFileSync(cdsrc, JSON.stringify({ requires: { db: { kind: 'hana', credentials } } }, null, 2))
+
+const vcap = path_join(process.cwd(), configPath, 'vcap.json')
+writeFileSync(vcap, JSON.stringify({ VCAP_SERVICES: { hana: [{ tags: ['hana'], credentials }] } }, null, 2))
